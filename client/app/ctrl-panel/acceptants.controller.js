88e5564c573ed55ccadc9c1706835276e3f1c3af
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-05-19 14:45:27 +0300

    Impl-ed getting data for the acceptant page.

--- client/app/ctrl-panel/acceptants.controller.js
+++ client/app/ctrl-panel/acceptants.controller.js
@@ -1,5 +1,26 @@
 'use strict';
 
 angular.module('armCtrlPanelApp')
-  .controller('AcceptantsCtrl', function ($scope) {
+  .controller('AcceptantsCtrl', function ($scope, myRest) {
+    /////////////////////////////////////////////////////////
+    // Initialization
+
+    $scope.acceptants = [];
+    updateAcceptants();
+
+    // Initialization
+    /////////////////////////////////////////////////////////
+
+    function updateAcceptants() {
+      myRest.getAllProviders().then(
+        function (providers) {
+          // TODO use real filtering here
+          //$scope.emitters = _.filter(providers, function (prov) {
+          //  return prov.meta[0] === "park 1";
+          //});
+          $scope.acceptants = providers;
+        }
+      );
+    }
+
   });
