bbed63372fca5c5ccc49c9d4a6d28b07fbdee018
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-05-19 17:02:59 +0300

    Small change.

--- client/app/ctrl-panel/services.controller.js
+++ client/app/ctrl-panel/services.controller.js
@@ -46,7 +46,7 @@ angular.module('armCtrlPanelApp')
         name: 'desc'
       },
       {
-        displayName: 'Под-услуга',
+        displayName: 'Детали',
         field: 'name'
       }
     ];
