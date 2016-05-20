c8746277847e5ebbd19cea1f7f6e9fa8a9960f55
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-05-20 10:22:09 +0300

    acceptants, emitters: impl-ed proper filtering, disp no data msg.

--- client/app/ctrl-panel/acceptants.controller.js
+++ client/app/ctrl-panel/acceptants.controller.js
@@ -14,11 +14,9 @@ angular.module('armCtrlPanelApp')
     function updateAcceptants() {
       myRest.getAllProviders().then(
         function (providers) {
-          // TODO use real filtering here
-          //$scope.emitters = _.filter(providers, function (prov) {
-          //  return prov.meta[0] === "park 1";
-          //});
-          $scope.acceptants = providers;
+          $scope.acceptants = _.filter(providers, function (prov) {
+            return prov.meta && prov.meta.isAcceptant;
+          });
         }
       );
     }
--- client/app/ctrl-panel/emitters.controller.js
+++ client/app/ctrl-panel/emitters.controller.js
@@ -14,11 +14,9 @@ angular.module('armCtrlPanelApp')
     function updateEmitters() {
       myRest.getAllProviders().then(
         function (providers) {
-          // TODO use real filtering here
-          //$scope.emitters = _.filter(providers, function (prov) {
-          //  return prov.meta[0] === "park 1";
-          //});
-          $scope.emitters = providers;
+          $scope.emitters = _.filter(providers, function (prov) {
+            return prov.meta && prov.meta.isEmitter;
+          });
         }
       );
     }
