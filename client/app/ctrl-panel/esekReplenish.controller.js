75d077b1935ce58cc5416319147b4515d8261cc2
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-06-08 16:47:00 +0300

    replenishment page: added time period selector.

--- client/app/ctrl-panel/esekReplenish.controller.js
+++ client/app/ctrl-panel/esekReplenish.controller.js
@@ -2,4 +2,63 @@
 
 angular.module('armCtrlPanelApp')
   .controller('EsekReplenishCtrl', function ($scope) {
+
+    //=======================================================
+    // Date range picker
+
+    $scope.datePicker = {};
+    $scope.datePicker.date = {
+      startDate: moment().startOf('day').subtract(1, 'months'), endDate: moment()
+    };
+    $scope.today = moment();
+
+    $scope.dateRangePickerOpts = {
+      eventHandlers: {
+        'apply.daterangepicker': function () {
+          //log("apply btn clicked");
+        },
+        'cancel.daterangepicker': function () {
+          //log("cancel btn clicked");
+        }
+      },
+      "locale": {
+        "applyLabel": "ОК",
+        "cancelLabel": "Отмена",
+        "customRangeLabel": "Выбрать"
+      },
+      ranges: {
+        'С начала недели': [moment().startOf('week'), moment()],
+        'С начала месяца': [moment().startOf('month'), moment()],
+        'За последние 30 дней': [moment().subtract(29, 'days').startOf('day'), moment()]
+      }
+    };
+
+    // Date range picker
+    //=======================================================
+
+    $scope.$watch('datePicker.date', function () {
+      log('time period changed');
+    });
+
+    
+
+  }) // controller
+
+  .filter('DatePickerFilter', function () {
+    return function (picker) {
+      const dateFrmt = 'DD.MM.YYYY';
+      let dtStart = picker.startDate;
+      let dtEnd = picker.endDate;
+      if (dtStart.isSame(dtEnd, 'day')) {
+        if (isToday(dtStart)) {
+          return 'сегодня';
+        }
+        else {
+          return dtStart.format(dateFrmt);
+        }
+      }
+      else {
+        return `период ${dtStart.format(dateFrmt)} - ${dtEnd.format(dateFrmt)}`;
+      }
+    };
   });
