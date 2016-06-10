c8588b93841d78921f4568ec69805c47a97e2810
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-06-10 10:33:33 +0300

    payments stat page: added a skeleton (no table yet).

--- client/app/ctrl-panel/esekPayment.controller.js
+++ client/app/ctrl-panel/esekPayment.controller.js
@@ -1,5 +1,64 @@
 'use strict';
 
 angular.module('armCtrlPanelApp')
-  .controller('EsekPaymentCtrl', function ($scope) {
+  .controller('EsekPaymentCtrl', function ($scope, myRest) {
+
+    $scope.data = {};
+
+    //=======================================================
+    // Date range picker
+
+    $scope.datePicker = {};
+    $scope.datePicker.date = {
+      startDate: moment().startOf('day').subtract(1, 'months'), endDate: moment()
+    };
+    $scope.today = moment();
+
+    $scope.dateRangePickerOpts = {
+      eventHandlers: {
+        'apply.daterangepicker': function () {
+          //log("apply btn clicked");
+        },
+        'cancel.daterangepicker': function () {
+          //log("cancel btn clicked");
+        }
+      },
+      "locale": {
+        "applyLabel": "ОК",
+        "cancelLabel": "Отмена",
+        "customRangeLabel": "Выбрать"
+      },
+      ranges: {
+        'С начала недели': [moment().startOf('week'), moment()],
+        'С начала месяца': [moment().startOf('month'), moment()],
+        'За последние 30 дней': [moment().subtract(29, 'days').startOf('day'), moment()]
+      }
+    };
+
+    // Date range picker
+    //=======================================================
+
+
+    function getData() {
+      $scope.isGettingData = true;
+      myRest.getStatReplenishment($scope.datePicker.date.startDate, $scope.datePicker.date.endtDate).then(
+        function (data) {
+          $scope.isGettingData = false;
+          $scope.data = data;
+          log(data);
+        },
+        function (reason) {
+          $scope.isGettingData = false;
+          $scope.data = {};
+        }
+      );
+    }
+
+    $scope.isObjectEmpty = isObjectEmpty;
+    $scope.getObjectPropNames = getObjectPropNames;
+
+    $scope.$watch('datePicker.date', function () {
+      getData();
+    });
+
   });
