be1a413ea0c3f88efda42e26f37cf4e4055bc744
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-06-23 10:34:56 +0300

    acceptants: added the ui-grid.

--- client/app/ctrl-panel/acceptants.controller.js
+++ client/app/ctrl-panel/acceptants.controller.js
@@ -1,9 +1,36 @@
 'use strict';
 
 angular.module('armCtrlPanelApp')
-  .controller('AcceptantsCtrl', function ($scope, $rootScope, myRest) {
+  .controller('AcceptantsCtrl', function ($scope, $rootScope, uiGridConstants, myRest) {
     /////////////////////////////////////////////////////////
     // Initialization
+
+    //-----------------------------------
+    // ui-grid setup
+
+    $scope.gridOptions = {};
+    $scope.gridOptions.columnDefs = [
+      {
+        displayName: 'Приложение',
+        field: 'app.title'
+      },
+      {
+        displayName: 'Акцептант',
+        field: 'title'
+      },
+      {
+        displayName: 'Описание',
+        field: 'description'
+      }
+    ];
+
+    $scope.gridOptions.enableHorizontalScrollbar = uiGridConstants.scrollbars.NEVER;
+    $scope.gridOptions.enableVerticalScrollbar = uiGridConstants.scrollbars.WHEN_NEEDED;
+    $scope.gridOptions.enableColumnMenus = false;
+    $scope.gridOptions.data = [];
+
+    // ui-grid setup
+    //-----------------------------------
 
     $scope.acceptants = [];
     updateAcceptants();
@@ -18,9 +45,11 @@ angular.module('armCtrlPanelApp')
           $scope.acceptants = _.filter(providers, function (prov) {
             return prov.meta && prov.meta.roles && _.includes(prov.meta.roles, "acceptant");
           });
+          $scope.gridOptions.data = $scope.acceptants;
           $rootScope.isGettingData = false;
         },
         function (reason) {
+          $scope.gridOptions.data = [];
           $rootScope.isGettingData = false;
         }
       );
